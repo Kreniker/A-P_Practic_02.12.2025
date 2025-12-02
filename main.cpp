5cd5cda15d3ab00c626f05a61a62bc697afe2eb9
Kreniker <kitserov.mike@gmail.com>
2025-12-02 09:46:44 +0300

    IDraw

--- main.cpp
+++ main.cpp
@@ -1,2 +1,34 @@
-int main()
-{}
+#include <iostream>
+
+namespace top {
+    struct p_t {
+        int x, y;
+    };
+
+    struct f_t {
+        p_t aa, bb;  
+    };
+
+    bool operator==(p_t a, p_t b);
+    bool operator!=(p_t a, p_t b);
+
+    struct IDraw {
+        virtual ~IDraw() = default;
+        virtual p_t next() const = 0;
+        virtual p_t next(p_t prev) const = 0;
+    };
+} 
+
+int main() {
+    using namespace top;
+    p_t a{1, 1}, b{1, 1};
+    std::cout << (a == b) << "\n";
+}
+
+bool top::operator==(p_t a, p_t b) {
+    return a.x == b.x && a.y == b.y;
+}
+
+bool top::operator!=(p_t a, p_t b) {
+    return !(a == b);
+}
