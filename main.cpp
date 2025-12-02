c6df19a3153a646850315e607fa33891b208e4ee
Kreniker <kitserov.mike@gmail.com>
2025-12-02 09:55:52 +0300

    class Dot

--- main.cpp
+++ main.cpp
@@ -14,15 +14,30 @@ namespace top {
 
     struct IDraw {
         virtual ~IDraw() = default;
-        virtual p_t next() const = 0;
+        virtual p_t begin() const = 0;
         virtual p_t next(p_t prev) const = 0;
     };
-} 
+    struct Dot: IDraw {
+    	p_t begin() const override;
+    	p_t next(p_t prev) const override;
+    	p_t d;
+    };
+}
 
 int main() {
     using namespace top;
     p_t a{1, 1}, b{1, 1};
     std::cout << (a == b) << "\n";
+}
+
+top::p_t top::Dot::begin() const {
+	return d;
+}
+top::p_t top::Dot::next(p_t prev) const {
+	if (prev != d){
+		throw std::logic_error("bad prev");
+	}
+	return d;
 }
 
 bool top::operator==(p_t a, p_t b) {
