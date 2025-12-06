96bc55a71ca48ab8280e15450a2868dac8e71962
Kreniker <kitserov.mike@gmail.com>
2025-12-06 17:02:18 +0300

    Homework: Square

--- main.cpp
+++ main.cpp
@@ -34,17 +34,25 @@ namespace top {
     	p_t start_point;
     	int len;
     };
+    struct Square : IDraw {
+    	explicit Square(p_t start, int ed);
+    	p_t begin() const override;
+    	p_t next(p_t prev) const override;
+    	p_t start_point;
+    	int edge;
+    };
     void append(const IDraw * sh, p_t ** ppts, size_t & s);
     f_t frame(const p_t * pts, size_t s);
     char * canvas(f_t fr, char fill);
     void paint(p_t p, char* cnv, f_t fr, char fill);
     void flush(std::ostream& os, const char * cnv, f_t fr);
+    
 }
 
 int main() {
 	using namespace top;
 	int err = 0;
-    IDraw* shp[4] = {};
+    IDraw* shp[5] = {};
     p_t * pts = nullptr;
     size_t s = 0;
     try{
@@ -52,7 +60,8 @@ int main() {
 	    shp [1] = new Dot({2, 3});
 	    shp [2] = new Dot({-5, -2});
 	    shp [3] = new Vline({-2, 0}, 5);
-		for (size_t i = 0; i < 4; ++i){
+	    shp [4] = new Square({1, 1}, 4);
+		for (size_t i = 0; i < 5; ++i){
 			append(shp[i], &pts, s);
 		}
 		f_t fr = frame(pts, s);
@@ -70,6 +79,7 @@ int main() {
     delete shp[2];
     delete shp[0];
     delete shp[3];
+    delete shp[4];
     return err;
 }
 
@@ -204,5 +214,55 @@ top::p_t top::Vline::next(p_t prev) const {
         return {prev.x, prev.y + 1};
     }
     
+    return start_point;
+}
+
+top::Square::Square(p_t start, int ed) :
+	start_point(start),
+	edge(ed)
+{
+	if (edge <= 0) {
+		throw std::logic_error("Edge lenght must be > 0");
+	}
+}
+
+top::p_t top::Square::begin() const {
+    return start_point;
+}
+top::p_t top::Square::next(p_t prev) const {    
+    if (edge == 1) {
+        return start_point;
+    }
+    
+    int dx = prev.x - start_point.x;
+    int dy = prev.y - start_point.y;
+    
+    if (dy == 0 && dx < edge - 1) {
+        return {prev.x + 1, prev.y};
+    }
+    
+    if (dx == edge - 1 && dy == 0) {
+        return {prev.x, prev.y + 1};
+    }
+    
+    if (dx == edge - 1 && dy < edge - 1) {
+        return {prev.x, prev.y + 1};
+    }
+    
+    if (dx == edge - 1 && dy == edge - 1) {
+        return {prev.x - 1, prev.y};
+    }
+    
+    if (dy == edge - 1 && dx > 0) {
+        return {prev.x - 1, prev.y};
+    }
+    
+    if (dx == 0 && dy == edge - 1) {
+        return {prev.x, prev.y - 1};
+    }
+    
+    if (dx == 0 && dy > 1) {
+        return {prev.x, prev.y - 1};
+    }
     return start_point;
 }
